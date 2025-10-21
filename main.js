361d4896fd45defaba62fcb8e9dfd36c0275c62d
Wahid-Muradctg <muneebatradersctg@gmail.com>
2025-10-21 21:59:19 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 let courser = document.querySelector('.courser');
-window.addEventListener('mousemove', (event)=>{
+window.addEventListener('mousemove', (event) => {
     let top = event.clientY;
     let left = event.clientX;
-    courser.style.top = top +'px';
-    courser.style.left = left +'px';
+    courser.style.top = top + 'px';
+    courser.style.left = left + 'px';
 })
 
 let togglerBtn = document.querySelector('.togglerBtn');
 let body = document.querySelector('body');
 let heading = document.querySelector('.toggleMood h2');
-togglerBtn.addEventListener('click', ()=>{
+togglerBtn.addEventListener('click', () => {
     body.classList.toggle('dark');
-    if(body.classList.contains('dark')){
+    if (body.classList.contains('dark')) {
         heading.innerHTML = ' Dark Mode';
-    }else{
+    } else {
         heading.innerHTML = 'Light Mode';
+        
     }
 });
 
@@ -22,21 +23,55 @@ let images = document.querySelectorAll('.imgStor img');
 let preview = document.querySelector('.preview');
 let previewImg = document.querySelector('.imgContainer img');
 let closeBtn = document.querySelector('.imgContainer .closeBtn');
-let leftBtn = document.querySelector('.leftBtn');
-let rightBtn = document.querySelector('.rightBtn');
-let i = 0
-images.forEach(img =>{
-    img.addEventListener('click',()=>{
+let leftBtn = document.querySelector('.imgContainer .leftBtn');
+let rightBtn = document.querySelector('.imgContainer .rightBtn');
+let previewIndex = 0;
+
+images.forEach((img, index) => {
+    img.addEventListener('click', () => {
         preview.classList.add('active');
-        
-        previewImg.src = img.src;
-    
-    })
+        previewIndex = index;
+        PreviewImage();
+    });
 });
 
-function closePreview(){
+function PreviewImage() {
+    previewImg.src = images[previewIndex].src;
+}
+
+function closePreview() {
     preview.classList.remove('active');
-};
+}
+
 closeBtn.addEventListener('click', closePreview);
-preview.addEventListener('click', closePreview);
+preview.addEventListener('click', (event) => {
+    if (event.target === preview) {
+        closePreview();
+    }
+});
+leftBtn.addEventListener('click', () => {
+
+    previewIndex = previewIndex - 1;
+
+    if (previewIndex < 0) {
+        previewIndex = images.length - 1;
+
+    }
+
+    PreviewImage();
+
+
+});
+rightBtn.addEventListener('click', () => {
+
+    previewIndex = previewIndex + 1;
+
+    if (previewIndex >= images.length) {
+        previewIndex = 0;
+    }
+
+    PreviewImage();
+
+});
+
 
